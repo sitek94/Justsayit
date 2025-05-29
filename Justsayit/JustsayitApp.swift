ae63b8abb823e0830b3d2863d25d0a17f4797f64
Maciek Sitkowski <msitkowski94@gmail.com>
2025-05-29 23:56:33 +0200

    init

---
+++ Justsayit/JustsayitApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustsayitApp.swift
+//  Justsayit
+//
+//  Created by Maciej Sitkowski on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct JustsayitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
