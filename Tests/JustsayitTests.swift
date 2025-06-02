ca53c2e1546c6c7f670bd96e3aba922ae78c3cf6
Maciek Sitkowski <msitkowski94@gmail.com>
2025-06-02 09:49:43 +0200

    init tests; add configs

---
+++ Tests/JustsayitTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustsayitTests.swift
+//  JustsayitTests
+//
+//  Created by Maciej Sitkowski on 02/06/2025.
+//
+
+import Testing
+@testable import Justsayit
+
+struct JustsayitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
