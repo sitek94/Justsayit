0eebaca58c8f9529b24ce19dacd7ac5fb4193e72
Maciek Sitkowski <msitkowski94@gmail.com>
2025-06-02 15:47:31 +0200

    use app name in build script

--- Scripts/build-app.sh
+++ Scripts/build-app.sh
@@ -6,18 +6,18 @@ version="$(cat $VERSION_FILE)"
 build_number="$GITHUB_RUN_NUMBER"
 
 # Create archive
-xcodebuild -project LearningSwift.xcodeproj -scheme LearningSwift \
+xcodebuild -project "$APP_NAME.xcodeproj" -scheme "$APP_NAME" \
   -configuration Release \
-  -archivePath build/LearningSwift.xcarchive \
+  -archivePath "build/$APP_NAME.xcarchive" \
   archive \
   MARKETING_VERSION="$version" \
   CURRENT_PROJECT_VERSION="$build_number"
 
 # Export the archive
 xcodebuild -exportArchive \
-  -archivePath build/LearningSwift.xcarchive \
+  -archivePath "build/$APP_NAME.xcarchive" \
   -exportPath "$BUILD_DIR/$XCODE_BUILD_PATH" \
-  -exportOptionsPlist Scripts/ExportOptions.plist
+  -exportOptionsPlist Resources/ExportOptions.plist
 
 # Verify the build
 file "$BUILD_DIR/$XCODE_BUILD_PATH/$APP_NAME.app/Contents/MacOS/$APP_NAME"
