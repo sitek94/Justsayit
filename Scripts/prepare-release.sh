83aa5ebfd4e0dc569d51a53638c022ba0e083b8c
Maciek Sitkowski <msitkowski94@gmail.com>
2025-06-02 16:10:43 +0200

    use correct path for appcast

--- Scripts/prepare-release.sh
+++ Scripts/prepare-release.sh
@@ -9,6 +9,6 @@ git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 
 # Commit appcast changes
-git add appcast.xml
+git add Resources/appcast.xml
 git commit -m "release v$version [skip ci]" || exit 0
 git push
